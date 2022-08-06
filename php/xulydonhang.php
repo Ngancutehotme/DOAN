247c664b9de3dc13fda21d8028ed559f72bb388e
NganVu <nganvu18102k@gmail.com>
2022-08-06 17:53:24 +0700

    :+1: Get chi tiet hoa don

--- php/xulydonhang.php
+++ php/xulydonhang.php
@@ -6,9 +6,18 @@ if (!isset($_POST['request']) && !isset($_GET['request'])) die(null);
 if ($_POST['request'] == 'getall') {
     $sql ="SELECT hd.*, nd.Ten, nd.GioiTinh, nd.SDT AS sdtND, nd.Email FROM `hoadon` AS hd
     LEFT JOIN nguoidung AS nd ON hd.MaND = nd.MaND";
-    $donhang = (new DB_driver())->get_list($sql);
+    $donHangs = (new DB_driver())->get_list($sql);
+    $result = [];
+    foreach ($donHangs as $donHang) {
+        $id = $donHang['MaHD'];
+        $sql ="SELECT cthd.SoLuong,cthd.DonGia, sp.TenSP, sp.HinhAnh FROM chitiethoadon AS cthd
+        INNER JOIN sanpham AS sp  ON cthd.MaSP = sp.MaSP WHERE cthd.MaHD=$id";
+        $chiTiet = (new DB_driver())->get_list($sql);
+        $donHang['thongTinChiTiet'] = $chiTiet;
+        array_push($result,$donHang);
+    }
 
-    die (json_encode($donhang));
+    die (json_encode($result));
 } else {
     die([]);
 }
